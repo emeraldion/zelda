2c8bac09e1086ee1ab0644aa4497267b55726398
claudiopro <claudio.procida@gmail.com>
2015-10-07 15:16:15 +0200

    Updates the about Claudio content

--- views/about/claudio.php
+++ views/about/claudio.php
@@ -1,29 +1,47 @@
 <?php
 	$this->set_title("Emeraldion Lodge .o. " . l('About Claudio'));
 ?>
-<h2>
-	<?php echo l("Who I am"); ?>
-</h2>
+
+<h2>About Claudio</h2>
 <p>
-	<?php printf(l("whoiam-para1"), date("Y", mktime(0, 0, 0, date("m") - 4, date("d") - 21, date("Y") - 1978)) - 2000); ?>
+	I&rsquo;m a thirty-something year old guy, living across Palermo, Italy and Dublin, Ireland.
+	Before living for a few years in Ireland, I spent a year in Spain during my uni, and soon
+	realized that I&rsquo;d like to live in too many places at once. Lack of ubiquity sucks!
+	Let&rsquo;s please invent teleportation soon.
 </p>
 <p>
-	<?php echo l("whoiam-para2"); ?>
+	I speak Italian, English and Spanish fluently, and know just a little of German, Turkish and Greek.
+	I enjoy hanging out with creative, lively, true people, as a result I&rsquo;m always rounded by geeks and artists.
+	I believe happiness is only real when shared, and life is all about emotions, creating bonds, and building great memories together.
+	I like unselfish friendship. Knowing different cultures and traditions. Expressing thoughts and ideas. Traveling, and enjoying the journey.
 </p>
 <p>
-	<?php echo l("whoiam-para3"); ?>
+	I made this site as a container for everything I make: my blog containing a mix of technical articles, rants, and
+	a few short stories, my art, my photos and my travel diaries. And also the projects I started or I&rsquo;m contributing to,
+	and the code I write.
 </p>
 
-<h2>
-	<?php echo l("What I like"); ?>
-</h2>
+<h2>What I do</h2>
 <p>
-	<?php echo l("whatilike-para1"); ?>
+	At the moment I&rsquo;m taking a short career break after a streak of seven years in the same job (gasp).
+	Catching up with the web development community, giving back the love.
+</p>
+<p>
+	From July 2008 to August 2015 I worked for IBM in Ireland across a few reorgs &mdashl; first in the Lotus brand,
+	then in the IBM Collaboration Solutions division, finally in the Enterprise Social Solutions division. During that time,
+	I developed enterprise social web applications with JavaScript, HTML and CSS and often dived in the guts of the platform coding with J2EE.
+	I went out giving talks to partners and customers, and participated into conferences.
+	I made great friends and created personal and professional bonds that last to day.
+</p>
+<p>
+	Before then, I had a blast developing Mac OS X applications in the long tail of the Delicious generation.
+	My utilities reached some popularity among Mac users and, even though I don&rsquo;t have time to maintain them anymore,
+	I still receive feedback from users from time to time.
+	I somewhat missed the chance to become a millionaire not joining the iOS development bandwagon early, but I don&rsquo;t regret it
+	as I much prefer the web, its open standards and awesome community to proprietary native apps.
 </p>
 
-<h2>
-	<?php echo l("Contact me"); ?>
-</h2>
+<h2>Reach out</h2>
 <p>
-	<?php echo l("writeme-para1"); ?>
+	If you like what I do and want to drop me a line, feel free to contact me. I love to know what people think about what I do.
 </p>
