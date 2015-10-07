ca8881231e8008bd765f32164a53f26458c8d1de
claudiopro <claudio.procida@gmail.com>
2015-10-07 16:47:52 +0200

    Added type property

--- helpers/opengraph.php
+++ helpers/opengraph.php
@@ -2,21 +2,52 @@
 
 	/**
 	 *	@class OpenGraph
-	 *	@short Defines methods and properties for the OpenGraph protocol.
+	 *	@short Defines methods and properties for the Open Graph protocol.
+   *  For a full description see http://ogp.me
 	 */
 	class OpenGraph
 	{
+    /**
+		 *	@attr description
+		 *	@short Value of the og:description tag
+		 */
 		public $description;
+
+    /**
+		 *	@attr image
+		 *	@short Value of the og:image tag
+		 */
     public $image;
+
+    /**
+		 *	@attr title
+		 *	@short Value of the og:title tag
+		 */
     public $title;
+
+    /**
+		 *	@attr url
+		 *	@short Value of the og:url tag
+		 */
     public $url;
 
+    /**
+		 *	@attr type
+		 *	@short Value of the og:type tag
+		 */
+    public $type;
+
+    /**
+     *  @fn OpenGraph
+     *  @short Constructs objects that hold Open Graph metadata
+     */
     public function OpenGraph($params)
     {
       $this->description = $params['description'];
       $this->image = $params['image'];
       $this->title = $params['title'];
       $this->url = $params['url'];
+      $this->type = $params['type'];
     }
 	}
 
--- views/_opengraph.php
+++ views/_opengraph.php
@@ -2,3 +2,4 @@
     <meta property="og:title" content="<?php print h($this->opengraph->title); ?>"/>
     <meta property="og:description" content="<?php print h($this->opengraph->description); ?>"/>
     <meta property="og:image" content="<?php print h($this->opengraph->image); ?>"/>
+    <meta property="og:type" content="<?php print h($this->opengraph->type); ?>"/>
