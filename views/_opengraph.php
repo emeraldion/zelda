bcfe6f1e402dc1f50a510c72b7ebef609e112b0a
claudiopro <claudio.procida@gmail.com>
2015-10-07 16:40:06 +0200

    Fixes bad markup

--- views/_opengraph.php
+++ views/_opengraph.php
@@ -1,4 +1,4 @@
-<meta property="og:url" content="<?php print h($this->opengraph->url); ?>"/>
-<meta property="og:title" content="<?php print h($this->opengraph->title); ?>"/>
-<meta property="og:description" content="<?php print h($this->opengraph->description); ?>"/>
-<meta property="og:image" content="<?php print h($this->opengraph->image); ?>"/>
+    <meta property="og:url" content="<?php print h($this->opengraph->url); ?>"/>
+    <meta property="og:title" content="<?php print h($this->opengraph->title); ?>"/>
+    <meta property="og:description" content="<?php print h($this->opengraph->description); ?>"/>
+    <meta property="og:image" content="<?php print h($this->opengraph->image); ?>"/>
--- views/layouts/about_layout.php
+++ views/layouts/about_layout.php
@@ -3,49 +3,37 @@
 	<head>
 		<title><?php print h($this->title); ?></title>
 <?php
-	require(dirname(__FILE__) . "/../views/_opengraph.php");
-	require(dirname(__FILE__) . "/../views/_styles.php");
+	require(dirname(__FILE__) . "/../views/_headers.php");
 ?>
 		<style type="text/css">
-			#primula_toggler
-			{
-				position: absolute;
-				top: 127px;
-				left: 240px;
-				cursor: pointer;
-			}
 		</style>
-<?php
-	require(dirname(__FILE__) . "/../views/_scripts.php");
-?>
 	</head>
 	<body>
-    <body>
   <?php
   //	require(dirname(__FILE__) . "/../views/_topbar.php");
   ?>
-      <header>
+		<header>
   <?php
   	require(dirname(__FILE__) . "/../views/_navbar.php");
   ?>
-      </header>
-      <main>
-        <div id="single-central" class="single-central">
-          <div class="center-column">
+		</header>
+		<main>
+      <div id="single-central" class="single-central">
+        <div class="center-column">
 <?php
 	// $this->render(array('partial' => 'navbar'));
 
 	print $this->content_for_layout;
 ?>
-          </div>
         </div>
       </div>
-    </main>
-    <footer>
+    </div>
+  </main>
+  <footer>
 <?php
 	require(dirname(__FILE__) . "/../views/_footer.php");
 ?>
-		</footer>
+	</footer>
 <?php
 	require(dirname(__FILE__) . "/../views/_scripts.php");
 ?>
