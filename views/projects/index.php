5396172df1471c63ed707af51f819244b1b258bc
claudiopro <claudio.procida@gmail.com>
2015-10-13 19:42:11 +0200

    Updates projects page

--- views/projects/index.php
+++ views/projects/index.php
@@ -1,6 +1,22 @@
 <?php
   $this->set_title('Emeraldion Lodge - ' . l('Projects'));
 ?>
+<h2>Projects</h2>
+<ul>
+  <li><a href="#learn-sass">learn-sass</a> &ndash; Learn SASS and SCSS through a workshopper adventure.</li>
+  <li><a href="#emerails">Emerails</a> &ndash; A lightweight Rails-like web application framework and ORM written in PHP.</li>
+  <li><a href="#mbl">MiniBatteryLogger</a> &ndash; A Mac OS X application designed to monitor the battery and log all power events.</li>
+  <li><a href="#mbl-archive">MiniBatteryLogger Shared Battery Data Archive</a> &mdash; A collection of battery stats, organized by computer and battery model, produced by MiniBatteryLogger.</li>
+  <li><a href="#project-mandarine">Project Mandarine</a> &mdash; A simple game implemented as a single-page JS application.</li>
+  <li><a href="#project-mandarine-cocoa">WebSMS</a> &mdash; A simple Cocoa + WebKit wrapper around the Project Mandarine game.</li>
+  <li><a href="#websms">WebSMS</a> &mdash; A widget to send free SMS messages through carriers&rsquo; web sites in a convenient way.</li>
+</ul>
+
+
+
+
+
+<a name="learn-sass"></a>
 <h2><a target="_blank" href="https://npmjs.org/package/learn-sass">learn-sass</a></h2>
 <h4>Learn SASS and SCSS through a workshopper adventure</h4>
 <p>
@@ -9,4 +25,108 @@
   <a target="_blank" href="http://nodeschool.io/">NodeSchool</a>.
   This adventure utilizes tutorials from the awesome
   <a target="_blank" href="http://sass-lang.com/guide">Sass guide</a>.
+</p>
+<p>
+  Check out the <a target="_blank" href="https://npmjs.org/package/learn-sass">NPM module</a>
+  and the source code on the
+  <a href="https://github.com/claudiopro/learn-sass" target="_blank">GitHub repo</a>.
+</p>
+
+
+
+<a name="emerails"></a>
+<h2><a target="_blank" href="//emerails.emeraldion.it">EmeRails</a></h2>
+<h4>A lightweight Rails-like web application framework and ORM written in PHP.</h4>
+<p>
+  A few years ago I wrote a lightweight clone of Ruby on Rails, by replicating much
+  of the ActiveRecord model class, and a convention-based MVC development model.
+  I struggled against the syntactical and practical limitations of the PHP language,
+  but at the end I had a working framework in less than a month.
+  EmeRails supports page caching, action filtering and a lot of useful features
+  that save coding time and server load.
+</p>
+<p>
+  Check out the <a href="//emerails.emeraldion.it" target="_blank">Demo instance</a>,
+  the <a href="//emerails.emeraldion.it/docs.html" target="_blank">Documentation</a>,
+  and the source code on the
+  <a href="https://github.com/emeraldion/emerails" target="_blank">GitHub repo</a>.
+</p>
+
+
+
+<a name="mbl"></a>
+<h2><a target="_blank" href="/software/macosx/minibatterylogger.html">MiniBatteryLogger</a></h2>
+<h4>A Mac OS X application designed to monitor the battery and log all power events.</h4>
+<p>
+  MiniBatteryLogger is a Mac OS X application that monitors laptop batteries,
+  traces the graph of charge and current over time, compares and rates the performance
+  of batteries among users, logs all power events and alerts the user with Growl notifications.
+  Since I am not currently maintaining this project, I released it as open source so people
+  can contribute and add support for new models and recent OSs.
+</p>
+<p>
+  Check out the <a href="/software/macosx/minibatterylogger.html" target="_blank">Application</a>,
+  and the source code on the
+  <a href="https://github.com/emeraldion/minibatterylogger" target="_blank">GitHub repo</a>.
+</p>
+
+
+
+<a name="mbl-archive"></a>
+<h2><a target="_blank" href="http://burgos.emeraldion.it/mbl/">MiniBatteryLogger Shared Battery Data Archive</a></h2>
+<h4>A collection of battery stats, organized by computer and battery model, produced by MiniBatteryLogger.</h4>
+<p>
+  The Shared Battery Data Archive is a collection of battery statistics, organized by computer model, for Apple
+  Macintosh laptop computers. It is a free resource for anybody to lookup, to evaluate the performance of own
+  batteries, to elaborate statistics on battery life, power consumption, etc. Data is presented hierarchically.
+  You can browse the list of models, which shows the number of available entries grouped by computer model,
+  and then choose a particular model to browse all the actual entries.
+</p>
+<p>
+  Check out the <a target="_blank" href="http://burgos.emeraldion.it/mbl/">Archive</a>.
+</p>
+
+
+
+<a name="project-mandarine"></a>
+<h2><a target="_blank" href="https://emeraldion.github.io/project-mandarine/">Project Mandarine</a></h2>
+<h4>A simple game implemented as a single-page JS application.</h4>
+<p>
+  Project Mandarine is a simple 2D game implemented with JavaScript. Sprites were hand drawn (sigh!)
+  and acquired with a scanner. The game location is a classroom at my high school :-).
+</p>
+<p>
+  Check out the source code on the
+  <a href="https://github.com/emeraldion/project-mandarine" target="_blank">GitHub repo</a>.
+</p>
+
+
+
+<a name="project-mandarine-cocoa"></a>
+<h2><a target="_blank" href="/software/macosx/project_mandarine.html">Project Mandarine Cocoa</a></h2>
+<h4>A simple Cocoa + WebKit wrapper around the Project Mandarine game.</h4>
+<p>
+  A native Cocoa application wrapper around the Project Mandarine game, rendering the JS application in a WebKitView.
+</p>
+<p>
+  Check out the <a href="/software/macosx/project_mandarine.html" target="_blank">Application</a>,
+  and the source code on the
+  <a href="https://github.com/emeraldion/project-mandarine-cocoa" target="_blank">GitHub repo</a>.
+</p>
+
+
+
+<a name="websms"></a>
+<h2><a target="_blank" href="/software/widgets/websms.html">WebSMS</a></h2>
+<h4>A widget to send free SMS messages through carriers&rsquo; web sites in a convenient way.</h4>
+<p>
+  WebSMS automates the tedious task of sending free text messages through the carriers&rsquo; websites,
+  which often require authenticating and clicking through a few pages. It is integrated with the
+  Address Book with autocompletion for easy access to recipients, and securely stores account passwords
+  in the system Keychain.
+</p>
+<p>
+  Check out the <a href="/software/widgets/websms.html" target="_blank">Widget</a>,
+  and the source code on the
+  <a href="https://github.com/emeraldion/websms" target="_blank">GitHub repo</a>.
 </p>
