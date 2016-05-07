107d568c753e9851fb0cf7a622cafee3a81ae2e9
claudiopro <claudio.procida@gmail.com>
2016-05-07 22:20:24 +0100

    Updates about page

--- views/about/claudio.php
+++ views/about/claudio.php
@@ -4,52 +4,15 @@
 
 <h2>About Claudio</h2>
 <p>
-	I&rsquo;m a thirty-something year old guy, living across Palermo, Italy and Dublin, Ireland.
-	Before living for a few years in Ireland, I spent a year in Spain during my uni, and soon
-	realized that I&rsquo;d like to live in too many places at once. Lack of ubiquity sucks!
-	Let&rsquo;s please invent teleportation soon.
+	Hey there, nice to meet you! I'm Claudio, a full stack software developer with a keen interest for the open web, privacy, security, and IoT.
+</p>
+<h3>What I do</h3>
+<p>
+  I'm currently making monitoring awesome at <a href="https://aws.amazon.com/" target="_blank">AWS</a>.
+  Previously, I made enterprise social awesome at <a href="https://www.ibm.com/social" target="_blank">IBM</a>.
+  I also took a short sabbatical between the two. Read more on my <a href="https://claudiopro.github.io/resume/" target="_blank">resume</a>.
 </p>
 <p>
-	I speak Italian, English and Spanish fluently, and know just a little of German, Turkish and Greek.
-	I enjoy hanging out with creative, lively, true people, as a result I&rsquo;m always rounded by geeks and artists.
-	I believe happiness is only real when shared, and life is all about emotions, creating bonds, and building great memories together.
-	I like unselfish friendship. Knowing different cultures and traditions. Expressing thoughts and ideas. Traveling, and enjoying the journey.
-</p>
-<p>
-	I truly believe in the value of the community, as a network of individuals to learn and be inspired from, and to work together to make
-	even the craziest ideas become real. I eagerly participate in local meetups, and often speak or mentor new speakers.
-	Over the years I came across and made friends with the most talented and brilliant individuals at meetups, conferences and events,
-	and am one of those who save for tickets for events just to be able to chat with fellow developers from all over the world.
-	Possibly in front of a beer :-D.
-<p>
-	I made this site as a container for everything I make: my blog containing a mix of technical articles, rants, and
-	a few short stories, my art, my photos and my travel diaries. And also the projects I started or I&rsquo;m contributing to,
-	and the code I write.
-</p>
-
-<h2>What I do</h2>
-<p>
-	At the moment I&rsquo;m taking a short career break after a streak of seven years in the same job (gasp).
-	Catching up with the web development community, giving back the love.
-</p>
-<p>
-	From July 2008 to August 2015 I worked for IBM in Ireland in the Lotus brand, which was later rebranded IBM Collaboration Solutions and
-	subsequently Enterprise Social Solutions division. During that time, I led a team that developed web applications for IBM&rsquo;s flagship
-	enterprise social solution, IBM Connections. I worked with JavaScript, HTML and CSS and often dived in the guts of the platform coding
-	with J2EE. During my last year I was given the opportunity to set the direction of the front-end for the entire product,
-	I also went out giving talks to partners and customers, and participated into conferences, user groups, and STEM events.
-	I made great friends and created personal and professional bonds that last to day.
-</p>
-<p>
-	Before then, I had a blast developing Mac OS X applications in the long tail of the Delicious generation.
-	My utilities reached some popularity among Mac users and, even though I don&rsquo;t have time to maintain them anymore,
-	I still receive feedback from users from time to time.
-	I somewhat missed the chance to become a millionaire not joining the iOS development bandwagon early, but I don&rsquo;t regret it
-	as I much prefer the web, its open standards and awesome community to proprietary native apps.
-</p>
-
-<h2>Reach out</h2>
-<p>
-	If you like what I do and want to drop me a line, feel free to <?php $this->link_to('contact me', array('controller' => 'contact')); ?>.
-	I love to know what people think about what I do.
+	I like being involved with the developers community and contributing to open source when I can (and my employer allows).
+  Find me at local meetups and tech conferences, I usually attend and sometimes speak too.
 </p>
