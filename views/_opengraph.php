bbbbb83b4afa1ac8360dfbac717ec8072ff208a7
claudiopro <claudio.procida@gmail.com>
2017-01-11 08:46:30 +0100

    Fixes padding in navbar, indentation in og metadata

--- views/_opengraph.php
+++ views/_opengraph.php
@@ -1,5 +1,5 @@
-    <meta property="og:url" content="<?php print h($this->opengraph->url); ?>"/>
-    <meta property="og:title" content="<?php print h($this->opengraph->title); ?>"/>
-    <meta property="og:description" content="<?php print h($this->opengraph->description); ?>"/>
-    <meta property="og:image" content="<?php print h($this->opengraph->image); ?>"/>
-    <meta property="og:type" content="<?php print h($this->opengraph->type); ?>"/>
+		<meta property="og:url" content="<?php print h($this->opengraph->url); ?>"/>
+		<meta property="og:title" content="<?php print h($this->opengraph->title); ?>"/>
+		<meta property="og:description" content="<?php print h($this->opengraph->description); ?>"/>
+		<meta property="og:image" content="<?php print h($this->opengraph->image); ?>"/>
+		<meta property="og:type" content="<?php print h($this->opengraph->type); ?>"/>
